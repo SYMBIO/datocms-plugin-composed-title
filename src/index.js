d0dd210bf159d82e0cba07eb359689c205df11ca
Martin Cajthaml <martin.cajthaml@symbio.agency>
2020-01-02 19:10:00 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 
 function getFieldValue(plugin, field) {
-  console.log(plugin.fields);
+  console.log(plugin.itemType);
   const fieldValue = plugin.getFieldValue(field);
   if (fieldValue) {
     if (typeof fieldValue === 'object' && Object.prototype.hasOwnProperty.call(fieldValue, plugin.locale)) {
